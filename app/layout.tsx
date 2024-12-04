e0a1520bd84e56f6f9206460ccd2735717c34bf6
Enzo Bruni <brunienzo@gmail.com>
2024-12-04 01:47:58 -0300

    Add `AnimatedBackground` component in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Press_Start_2P } from 'next/font/google';
+import AnimatedBackground from './components/AnimatedBackground';
 import './globals.css';
 
 const pressStart2P = Press_Start_2P({ subsets: ['latin'], weight: '400' });
@@ -17,8 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${pressStart2P.className} bg-black text-white`}>
-        {children}
+        <AnimatedBackground />
+        <div className="relative z-10">
+          {children}
+        </div>
       </body>
     </html>
-  );
+  )
 }
+
